65cffae978037dc1e98c96b51d402fb2af8b81f3
Zion Ng <zion@intzone.com>
2012-12-06 19:11:58 +0800

    Rewritten to run using XML config

---
+++ phpunit_browser/README.md
@@ -0,0 +1,20 @@
+Run PHPUnit in Browser
+======================
+
+**Purpose**<br />
+Run PHPUnit in browser
+
+**Example**<br />
+1. Clone this project into your <webroot>/test.
+2. Type the following in your browser: http://localhost/test/phpunit_browser.php
+3. The script will load PersonTest.php (which uses Person.php)
+4. You should see the following output (or similar) in your browser:
+```
+  PHPUnit 3.7.10 by Sebastian Bergmann.
+
+.
+
+Time: 0 seconds, Memory: 3.00Mb
+
+OK (1 test, 1 assertion)
+```
---
+++ phpunit_browser/phpunit_browser.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Runs PHPUnit command in browser using phpunit.xml for configuration
+ *
+ * Useful if there is no access to commandline, eg. restricted permissions
+ * Assuming this file and phpunit.xml are in <webroot>/test, just type this in the browser:
+ *     http://localhost/test/phpunit_browser.php
+ *
+ * @author Zion Ng <zion@intzone.com>
+ * @link   [PHPUnit] https://github.com/sebastianbergmann/phpunit/
+ * @link   [Source] https://github.com/zionsg/standalone-php-scripts/tree/master/phpunit_browser
+ * @since  2012-12-06T19:00+08:00
+ */
+
+// Assumes PHPUnit has been installed via Composer and is in include path
+require_once 'PHPUnit/vendor/autoload.php';
+$command = new PHPUnit_TextUI_Command;
+$arguments = explode(' ', '-c phpunit.xml');
+?>
+<pre>
+<?php $command->run($arguments, true); // capture output in <pre> tags ?>
+</pre>
