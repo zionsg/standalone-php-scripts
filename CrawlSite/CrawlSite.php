2ac020a2269bf704205824dd4044aba8677e0110
Zion Ng <zion@intzone.com>
2013-11-21 17:06:10 +0800

    Use DOMDocument instead of regex to crawl sites

--- CrawlSite/CrawlSite.php
+++ CrawlSite/CrawlSite.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * Crawl website for links using downwards traversal only
+ * Crawl website using downwards traversal only
  *
- * For each webpage crawled, a local copy with renamed links will be saved.
+ * For each webpage crawled, a local copy with renamed links to webpages will be saved.
  *
  * Usage:
  *     $crawler = new CrawlSite();
  *     $links = $crawler('http://example.com/test/');
  *     echo '<pre>' . print_r($links, true) . '</pre>';
  *
- * @see     http://nadeausoftware.com/articles/2008/05/php_tip_how_convert_relative_url_absolute_url for url_to_absolute
  * @author  Zion Ng <zion@intzone.com>
  * @link    [Source] https://github.com/zionsg/standalone-php-scripts/tree/master/CrawlSite
  * @since   2013-11-06T19:00+08:00
@@ -17,6 +16,10 @@
 
 include 'UrlToAbsolute/url_to_absolute.php';
 
+/**
+ * @link http://nadeausoftware.com/articles/2008/05/php_tip_how_convert_relative_url_absolute_url for url_to_absolute
+ * @link http://htmlparsing.com/php.html on why DOMDocument is used instead of regular expression
+ */
 class CrawlSite
 {
     /**
@@ -36,11 +39,39 @@ class CrawlSite
     protected $defaultExtension = 'php';
 
     /**
+     * Path to directory where site resides in - for determining downward links
+     *
+     * @var string
+     */
+    protected $siteDir = '';
+
+    /**
      * CURL Handler
      *
      * @var resource
      */
     protected $curlHandler;
+
+    /**
+     * Tracking list for processed files
+     *
+     * @var array
+     */
+    protected $processed = array();
+
+    /**
+     * Queue
+     *
+     * @var array
+     */
+    protected $queue = array();
+
+    /**
+     * Links
+     *
+     * @var array
+     */
+    protected $links = array();
 
     /**
      * Constructor
@@ -74,50 +105,43 @@ class CrawlSite
     }
 
     /**
-     * Crawl site for links using downwards traversal only
+     * Crawl site using downwards traversal only
      *
-     * For each webpage crawled, a local copy with renamed links will be saved.
+     * For each webpage crawled, a local copy with renamed links to webpages will be saved.
+     * The renamed links will be converted to absolute links for easy naming of local copies.
+     *
+     * Only meta refresh links and <a> href links are crawled.
      *
      * @param  string $site
      * @return array  array('webpage' => array(link1, link2, ...))
      */
     function __invoke($site)
     {
-        // Regex pattern for link types - meta refresh tags need to be processed else may stop at index page
-        // Need to check whitespace in front of href/src else Javascript vars such as x.src or x.oSrc will be captured
-        // Backreferences to match single/double quotes not used as they end up capturing more than the link
-        $pattern = '~'
-                 . '<[\s]*meta.+refresh.+content=".*url=([^"]+)"'    // double quotes
-                 . '|<[\s]*meta.+refresh.+content=\'.*url=([^\']+)\'' // single quotes
-                 . '|[\s]href="([^"]+)"'            // for links with double quotes
-                 . '|[\s]href=\'([^\']+)\''         // for links with single quotes
-                 . '|[\s]href=([^\'"][^\s]+)[^>]*>' // for links without quotes
-                 . '|[\s]src="([^"]+)"'             // for links with double quotes
-                 . '|[\s]src=\'([^\']+)\''          // for links with single quotes
-                 . '|[\s]src=([^\'"][^\s]+)[^>]*>'  // for links without quotes
-                 . '~i';
+        $dom = new DOMDocument();
 
-        // Get path to directory when $site resides in - for determining downward links
+        // Get path to directory where $site resides in - for determining downward links
         // parse_url() used else http://example.com will give ".com" extension
         if (pathinfo(parse_url($site, PHP_URL_PATH), PATHINFO_EXTENSION)) {
-            $siteDir = pathinfo($site, PATHINFO_DIRNAME) . '/';
+            $this->siteDir = pathinfo($site, PATHINFO_DIRNAME) . '/';
         } else {
             $site = rtrim($site, "\\/") . '/';
-            $siteDir = $site;
+            $this->siteDir = $site;
         }
 
+        // Clear any previous work
         clearstatcache();
-        $queue = array($site => $site); // $site is used as key to prevent duplicates
-        $processed = array(); // keep tracked of processed links
-        $links = array();
-        while (!empty($queue)) {
-            $url = array_shift($queue);
-            $processed[$url] = true;
+        $this->queue = array($site => $site); // $site is used as key to prevent duplicates
+        $this->processed = array();
+        $this->links = array();
+
+        while (!empty($this->queue)) {
+            $url = array_shift($this->queue);
+            $this->processed[$url] = true;
 
             // Only process downward links
             // "/" is appended to prevent http://example.com/test from being matched in http://example.com/test123
             // and to ensure http://example.com will match http://example.com/
-            if (stripos($url . '/', $siteDir) === false) {
+            if (stripos($url . '/', $this->siteDir) === false) {
                 continue;
             }
 
@@ -140,62 +164,31 @@ class CrawlSite
                 continue;
             }
 
-            // Find links
-            $matches = array();
-            if (!preg_match_all($pattern, $contents, $matches, PREG_OFFSET_CAPTURE)) {
-                continue;
+            // Parse HTML - @ suppresses any warnings that loadHTML might throw because of invalid HTML in the page
+            @$dom->loadHTML($contents);
+
+            // Need to check for meta refresh tags else crawling may stop at index page
+            foreach ($dom->getElementsByTagName('meta') as $element) {
+                if ($element->getAttribute('http-equiv') != 'refresh') {
+                    continue;
+                }
+                $matches = array();
+                if (!preg_match('~^(.*)url=(.*)$~i', $element->getAttribute('content'), $matches)) {
+                    continue;
+                }
+                $link = $this->processLink($matches[2], $url);
+                $element->setAttribute('content', $matches[1] . $link);
             }
 
-            // Filter out empty matches and order according to offset
-            // Even capturing groups for quote marks must be included else position offsets will be affected
-            $orderedMatches = array();
-            $matchCnt = count($matches);
-            for ($i = 1; $i < $matchCnt; $i++) {
-                foreach ($matches[$i] as $match) {
-                    if (!is_array($match) || !$match[0]) {
-                        continue;
-                    }
-                    $orderedMatches[$match[1]] = $match[0];
-                }
-            }
-            ksort($orderedMatches);
-
-            // Find and rename downward links in page contents
-            $prevOffset = 0;
-            $newContents = '';
-            foreach ($orderedMatches as $offset => $link) {
-                // url_to_absolute does not handle files with spaces in their names, hence replace with %20
-                // $link is not modified as it will affect the position offsets
-                $isJavascript = (substr($link, 0, 11) == 'javascript:');
-                if ($isJavascript) {
-                    $absoluteLink = $link;
-                } else {
-                    $absoluteLink = str_replace(
-                        array('%3F', '%3D'),
-                        array('?', '='),
-                        url_to_absolute($url, str_replace(' ', '%20', $link))
-                    );
-                }
-                $linkLen = strlen($link);
-
-                if (stripos($absoluteLink, $siteDir) !== false) {
-                    $renamedLink  = $this->renameUrl($absoluteLink);
-                } else {
-                    $renamedLink = $absoluteLink;
-                }
-                $newContents .= substr($contents, $prevOffset, $offset - $prevOffset) . $renamedLink;
-                $prevOffset   = $offset + $linkLen;
-
-                if (!isset($processed[$absoluteLink]) && !$isJavascript) {
-                    $queue[$absoluteLink] = $absoluteLink;
-                }
-                $links[$url][] = $absoluteLink;
+            // <a> tags
+            foreach ($dom->getElementsByTagName('a') as $element) {
+                $link = $this->processLink($element->getAttribute('href'), $url);
+                $element->setAttribute('href', $link);
             }
 
             // Save local copy - error may occur if directory nesting is too deep or if path is too long
-            $newContents .= substr($contents, $prevOffset);
-            $renamedUrl   = $this->renameUrl($url);
-            $filename     = preg_replace(
+            $renamedUrl = $this->renameUrl($url);
+            $filename   = preg_replace(
                 '~^(.*:[\\/]+)~',
                 str_replace("\\", '/', getcwd()) . '/',
                 $renamedUrl
@@ -204,12 +197,54 @@ class CrawlSite
             if (!file_exists($dir)) {
                 mkdir($dir, 0777, true);
             }
-            file_put_contents($filename, $newContents);
+            $dom->saveHTMLFile($filename);
 
         } // end while
 
-        return $links;
+        return $this->links;
     } // end function __invoke
+
+    /**
+     * Process link including queueing and renaming it
+     *
+     * Only webpages will be queued and renamed to absolute paths
+     *
+     * @param  string $link
+     * @param  string $baseUrl
+     * @return string Renamed link
+     */
+    protected function processLink($link, $baseUrl)
+    {
+        $extension = pathinfo(parse_url($link, PHP_URL_PATH), PATHINFO_EXTENSION);
+
+        if (trim($link) == '') { // Empty link
+           $renamedLink = $link;
+        } elseif (substr($link, 0, 11) == 'javascript:') { // Javascript links
+            $renamedLink = $link;
+        } elseif ($extension && !in_array($extension, $this->pageExtensions)) { // Non-webpages such as .css, .js, .jpg
+            $renamedLink = $link;
+        } else { // Webpages
+            // url_to_absolute cannot handle spaces in paths, hence replacing with %20
+            $link = str_replace(
+                array('%3F', '%3D'),
+                array('?', '='),
+                url_to_absolute($baseUrl, str_replace(' ', '%20', $link))
+            );
+
+            if (stripos($link, $this->siteDir) !== false) {
+                $renamedLink  = $this->renameUrl($link);
+            } else {
+                $renamedLink = $link;
+            }
+
+            if (!isset($this->processed[$link])) {
+                $this->queue[$link] = $link;
+            }
+        }
+
+        $this->links[$baseUrl][] = $link;
+        return $renamedLink;
+    }
 
     /**
      * Renames url with query string to filesystem friendly url
