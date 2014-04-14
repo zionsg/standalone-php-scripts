7763a645cf80ed0fe4b89c53b912c3ee2cff9d19
Zion Ng <zion@intzone.com>
2014-04-14 10:31:03 +0800

    Create npp_convert_utf8.py

---
+++ CrawlSite/npp_convert_utf8.py
@@ -0,0 +1,30 @@
+# Python script for bulk conversion of HTML and PHP files to UTF-8 without BOM using Notepad++
+#
+# @author Zion Ng <zion@intzone.com>
+# @link   [Source] https://github.com/zionsg/standalone-php-scripts/tree/master/CrawlSite
+# @see    http://notepad-plus-plus.org/ for Notepad++
+# @see    http://npppythonscript.sourceforge.net/ for PythonScript plugin for Notepad++
+#
+
+import os;
+import sys;
+
+# Modify path and extensions of files to convert here
+path = 'path/to/files'
+extensions = ['.htm', '.html', '.php', '.phtml']
+
+# processDirectory() will be called recursively for each directory encountered
+os.path.walk(path, processDirectory, None)
+
+# Function to convert files in dir to UTF-8 without BOM using Notepad++
+def processDirectory(args, dir, files):
+    console.write('Directory: ' + dir + "\r\n")
+    for file in files:
+      for extension in extensions:
+        if file[-len(extension):] == extension:
+          console.write('    ' + file + "\r\n")
+          notepad.open(dir + "\\" + file)
+          notepad.runMenuCommand('Encoding', 'Convert to UTF-8 without BOM')
+          notepad.save()
+          notepad.close()
+          break
